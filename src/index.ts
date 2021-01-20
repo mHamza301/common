ba3dd5530994b78325db198a6d76f79aeae311c0
mHamza301 <mHamza301@users.noreply.github.com>
2021-01-20 20:49:14 +0500

    Additional Config

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+interface ColorCode {
+    red: number;
+    green: number;
+    blue: number;
+}
+
+const color: ColorCode  = {
+    red: 10,
+    green: 20,
+    blue: 30
+};
+
+console.log(color);
